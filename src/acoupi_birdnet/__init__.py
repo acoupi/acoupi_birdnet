8337a5fe1fc116d5ebd3d4fd21e9b255740c2afa
mbsantiago <santiago.mbal@gmail.com>
2024-05-21 18:29:50 +0100

    Make sure test are passing

--- src/acoupi_birdnet/__init__.py
+++ src/acoupi_birdnet/__init__.py
@@ -1,7 +1,7 @@
 from acoupi_birdnet.model import BirdNET
-from acoupi_birdnet.program import BirdNET_Program
+from acoupi_birdnet.program import BirdNETProgram
 
 __all__ = [
     "BirdNET",
-    "BirdNET_Program",
+    "BirdNETProgram",
 ]
